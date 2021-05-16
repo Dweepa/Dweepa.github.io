773933b81d17177003cab91e8e9711c108015e2d
Dweepa <dweepa.prasad@gmail.com>
2021-05-16 13:44:30 +0530

    Add scripts, styles and assets

---
+++ script/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+  // hide the other tabs on load
+  $("#educationDetail").hide();
+  $("#publicationDetail").hide();
+
+  $("#education").click(function(){
+    $(".detail").html("Education");
+    $("#educationDetail").hide();
+    $("#experienceDetail").hide();
+    $("#publicationDetail").hide();
+    $("#educationDetail").show("slow","linear"); // show only education
+  });
+  $("#experience").click(function(){
+    $(".detail").html("Experience");
+    $("#experienceDetail").hide();
+    $("#educationDetail").hide();
+    $("#publicationDetail").hide();
+    $("#experienceDetail").show("slow", "linear"); // show only experience
+  });
+  $("#publications").click(function(){
+    $(".detail").html("Publications");
+    $("#educationDetail").hide();
+    $("#publicationDetail").hide();
+    $("#experienceDetail").hide();
+    $("#publicationDetail").show("slow", "linear"); // show only publication
+  });
+  $("#github").click(function(){
+    window.open("https://github.com/Dweepa");
+  });
+  $("#linkedin").click(function(){
+    window.open("https://www.linkedin.com/in/dweepa-honnavalli/");
+  });
+});

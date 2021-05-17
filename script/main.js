fbf9932c83821ec32083dd58af39f83f1736adf7
Dweepa <dweepa.prasad@gmail.com>
2021-05-17 08:02:13 +0530

    Update styles and scripts

--- script/main.js
+++ script/main.js
@@ -1,34 +1,54 @@
 $(document).ready(function(){
-
   // hide the other tabs on load
-  $("#experienceDetail").hide();
-  $("#publicationDetail").hide();
+  hideAll();
+  $("#educationtab").fadeIn("slow");
 
   $("#education").click(function(){
-    $(".detail").html("Education");
-    $("#educationDetail").hide();
-    $("#experienceDetail").hide();
-    $("#publicationDetail").hide();
-    $("#educationDetail").show("slow","linear"); // show only education
+    $(".tab").html("Education");
+    hideAll();
+    $("#educationtab").fadeIn("slow");
+    changeTitle("Dweepa H | Education");
   });
   $("#experience").click(function(){
-    $(".detail").html("Experience");
-    $("#experienceDetail").hide();
-    $("#educationDetail").hide();
-    $("#publicationDetail").hide();
-    $("#experienceDetail").show("slow", "linear"); // show only experience
+    $(".tab").html("Experience");
+    hideAll();
+    $("#experiencetab").fadeIn("slow");
+    changeTitle("Dweepa H | Experience");
   });
   $("#publications").click(function(){
-    $(".detail").html("Publications");
-    $("#educationDetail").hide();
-    $("#publicationDetail").hide();
-    $("#experienceDetail").hide();
-    $("#publicationDetail").show("slow", "linear"); // show only publication
+    $(".tab").html("Publications");
+    hideAll();
+    $("#publicationtab").fadeIn("slow", "linear");
+    changeTitle("Dweepa H | Publications");
   });
+  $("#coursebutton").click(function(){
+    $(".tab").html("Courses");
+    hideAll();
+    $("#coursetab").fadeIn("slow", "linear");
+    changeTitle("Dweepa H | Course Projects")
+  });
+
+  // external links
   $("#github").click(function(){
     window.open("https://github.com/Dweepa");
   });
   $("#linkedin").click(function(){
     window.open("https://www.linkedin.com/in/dweepa-honnavalli/");
   });
+
+  $(".square-button").hover(function(){
+    $(this).html("Watch Video");
+  });
+
+
+  function hideAll(){
+    $("#coursetab").hide();
+    $("#educationtab").hide();
+    $("#publicationtab").hide();
+    $("#experiencetab").hide();
+  }
 });
+
+function changeTitle(title){
+  document.title=title
+}
